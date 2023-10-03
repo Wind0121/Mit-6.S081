b1475bf66e01b009c98c582cd4e72c93cd0d3eab
kezzy <1561081334@qq.com>
2023-10-03 15:57:05 +0800

    完成Lab6 COW

--- kernel/trap.c
+++ kernel/trap.c
@@ -65,7 +65,14 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  }else if(r_scause() == 12 || r_scause() == 13 || r_scause() == 15){
+    uint64 fault_va = r_stval();
+    if(fault_va >= p->sz
+        || cowpage(p->pagetable,fault_va) != 0
+        || cowalloc(p->pagetable,PGROUNDDOWN(fault_va)) == 0){
+        p->killed = 1;
+    }
+  }else if((which_dev = devintr()) != 0){
     // ok
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
--- kernel/vm.c
+++ kernel/vm.c
@@ -311,7 +311,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+//  char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -320,13 +320,21 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
+
+    if(flags & PTE_W){
+        flags &= ~PTE_W;
+        flags |= PTE_F;
+        *pte  = PA2PTE(pa) | flags;
     }
+
+//    if((mem = kalloc()) == 0)
+//      goto err;
+//    memmove(mem, (char*)pa, PGSIZE);
+
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+        goto err;
+    }
+    kaddrefcnt((char*)pa);
   }
   return 0;
 
@@ -361,6 +369,9 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
+    if(cowpage(pagetable,va0) == 0){
+        pa0 = (uint64)cowalloc(pagetable,va0);
+    }
     n = PGSIZE - (dstva - va0);
     if(n > len)
       n = len;
@@ -440,3 +451,4 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
